d219379b50d77dbc84ccf10c26c7bafaaefbb7d0
SignalPty <signalpatrick1@gmail.com>
2025-05-17 11:18:17 +0200

    Add files via upload

---
+++ list.py
@@ -0,0 +1,5 @@
+
+name_list = ["Kagatera", "Johan", "Peter", "Alext Clude"]
+name_list.sort(reverse=True)
+name_list.extend(["Patrick", "university of Kigali ", "NTAKIRUTIMANA"])
+print("Final sorted list:", name_list)
